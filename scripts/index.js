c21ec4a463696397ad61b0bfb9f91c03a201dd33
Blake C <blake.cameron1001@gmail.com>
2023-04-30 00:55:43 +1200

    update modal

--- scripts/index.js
+++ scripts/index.js
@@ -27,9 +27,12 @@ const initialCards = [
 
 const profileEditButton = document.querySelector("#profile-edit-button");
 const profileEditModal = document.querySelector("#profile-edit-modal");
+const modalCloseButton = document.querySelector("#modal-close-button");
 
 profileEditButton.addEventListener("click", () => {
   profileEditModal.classList.add("modal__opened");
 });
 
-const modalCloseButton = document.querySelector;
+modalCloseButton.addEventListener("click", () => {
+  profileEditModal.classList.remove("modal__opened");
+});
